545e5dc726002bdc9421cddf5b6544afe1a4aee9
the5loth <39337041+the5loth@users.noreply.github.com>
2018-05-16 14:25:53 +0100

    Update .genmaster.sh

--- downloads/.genmaster.sh
+++ downloads/.genmaster.sh
@@ -5,4 +5,4 @@ set -ue
 openssl genpkey -out .server/master.pem -algorithm rsa -pkeyopt rsa_keygen_bits:4096
 
 # Generate the corresponding public key. It is embedded to the client.
-openssl pkey -in .server/master.pem -out client/master.pem -pubout
+openssl pkey -in .server/master.pem -out .client/master.pem -pubout
